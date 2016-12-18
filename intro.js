8a2af4107e0713d8dba004dfccd0c5640c4a5167
jchua1 <jchua@stuy.edu>
2016-12-18 14:34:29 -0500

    part 2

--- intro.js
+++ intro.js
@@ -2,6 +2,8 @@ var b = document.getElementById('b');
 var l = document.getElementById('thelist');
 var li = document.getElementsByTagName('li');
 var h = document.getElementById('h');
+var fib = document.getElementById('fib');
+var bfib= document.getElementById('bfib');
 
 function addEvents(item) {
     item.addEventListener('mouseover', function() {
@@ -25,3 +27,19 @@ b.addEventListener('click', function() {
 for (var i = 0; i < li.length; i++) {
     addEvents(li[i]);
 }
+
+function fibonacci(n) {
+    if (n == 0)
+	return 0;
+    else if (n == 1)
+	return 1;
+    else
+	return fibonacci(n-1) + fibonacci(n-2);
+}
+
+bfib.addEventListener('click', function() {
+    var length = fib.getElementsByTagName('li').length + 1;
+    var item = document.createElement('li');
+    item.innerHTML = fibonacci(length);
+    fib.appendChild(item);
+});
