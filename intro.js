798731093b669e47cd18dc4765569f8ea95b31f6
jchua1 <jchua@stuy.edu>
2016-12-18 14:03:26 -0500

    part 1

--- intro.js
+++ intro.js
@@ -1,16 +1,27 @@
 var b = document.getElementById('b');
 var l = document.getElementById('thelist');
-var li = document.getElementById('li');
+var li = document.getElementsByTagName('li');
 var h = document.getElementById('h');
+
+function addEvents(item) {
+    item.addEventListener('mouseover', function() {
+	h.innerHTML = item.innerHTML;
+    });
+    item.addEventListener('mouseout', function() {
+	h.innerHTML = 'Hello World!';
+    });
+    item.addEventListener('click', function() {
+	item.remove();
+    });
+}
 
 b.addEventListener('click', function() {
     var item = document.createElement('li');
     item.innerHTML = 'item';
+    addEvents(item);
     l.appendChild(item);
 });
     
 for (var i = 0; i < li.length; i++) {
-    li[i].addEventListener('mouseover', function() {
-	h.innerHTML = this.innerHTML;
-    });
+    addEvents(li[i]);
 }
